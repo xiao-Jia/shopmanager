a94450a92587828808231e41d428a193b49c8f46
xiao-Jia <951988214@qq.com>
2019-02-15 21:03:30 +0800

    引入登陆组件

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>login---</div>
+  <div>
+    <el-form label-position="top" label-width="80px" :model="formdata">
+        <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button type="success">成功按钮</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+    data(){
+        return{
+            formdata:{
+                username:'',
+                password:''
+            }
+        }
+    }
+};
 </script>
 
 <style>
-
 </style>
