a8c4fd20c4feb3691488d420cb7a40f6eacdb5c3
xiao-Jia <951988214@qq.com>
2019-02-15 20:38:53 +0800

    登陆路由配置完毕

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/components/login.vue'
+// webpack -> @ -> src
 Vue.use(Router)
 
 export default new Router({
@@ -8,6 +9,10 @@ export default new Router({
     {
       path: '/',
       name: 'home'
+    }, {
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
