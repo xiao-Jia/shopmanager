1edaf82c939ca98471d87336d9d8b1e435f9a71f
xiao-Jia <951988214@qq.com>
2019-02-16 11:25:34 +0800

    头部调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container class="container">
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+            <img src="@/assets/logo.png" alt="图片加载失败">
+        </el-col>
+        <el-col class="middle" :span="19">
+            <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+            <a class="logout" href="#">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside class="aside" width="200px">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -14,14 +26,22 @@ export default {};
 
 <style>
 .container {
-background-color: deeppink;
-    height: 100%;
-
+  background-color: #B3C0D1;
+  height: 100%;
 }
-.aside{
-    background-color: skyblue;
+.aside {
+  background-color: skyblue;
 }
-.main{
-    background-color: gold;
+.main {
+  background-color: gold;
+}
+.middle{
+    text-align: center;
+    line-height: 60px;
+    color: #fff;
+}
+.logout{
+    text-align: center;
+    line-height: 60px;
 }
 </style>
