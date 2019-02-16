de6efd42c06af7048852230574102322e935cf98
xiao-Jia <951988214@qq.com>
2019-02-16 09:38:56 +0800

    处理了登陆请求res

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,30 +1,64 @@
 <template>
-  <div>
-    <el-form label-position="top" label-width="80px" :model="formdata">
-        <h2>用户登录</h2>
+  <div class="login-wrap">
+    <el-form class="login-from" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
       <el-form-item label="用户名">
         <el-input v-model="formdata.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
+      <el-button
+      @click.prevent='handleLogin'
+      class="login-btn" type="success">登陆</el-button>
     </el-form>
-    <el-button type="success">成功按钮</el-button>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            formdata:{
-                username:'',
-                password:''
-            }
-        }
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
     }
-};
+  },
+  methods: {
+    handleLogin () {
+      this.$http.post(`login`, this.formdata).then(res => {
+        console.log(res)
+        const{data:{data,meta:{msg,status}}}= res
+        if (status === 200){
+
+        } else {
+          this.$message.error(msg);
+        }
+      })
+      .catch(err=>{
+        console.log(err)
+      })
+    }
+  }
+}
 </script>
 
 <style>
+.login-wrap {
+  background: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-from {
+    background: #fff;
+    width: 400px;
+    border-radius: 5px;
+    padding: 30px;
+}
+.login-btn{
+    width: 100%;
+}
 </style>
