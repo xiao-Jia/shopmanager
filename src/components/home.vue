e5003d2c97d2fad92bae2e37fc70277d498adaa5
xiao-Jia <951988214@qq.com>
2019-02-16 11:10:26 +0800

    引入布局容器

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>home---</div>
+  <el-container class="container">
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.container {
+background-color: deeppink;
+    height: 100%;
 
+}
+.aside{
+    background-color: skyblue;
+}
+.main{
+    background-color: gold;
+}
 </style>
