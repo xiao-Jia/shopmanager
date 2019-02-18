8cc94d9f7862f3042ff4beee65e63edb0611899d
xiao-Jia <951988214@qq.com>
2019-02-18 23:30:40 +0800

    请求拦截器统一处理请求头

--- src/http.js
+++ src/http.js
@@ -4,9 +4,26 @@ const HttpServer = {};
 
 HttpServer.install = function (Vue) {
     axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
-    const AUTH_TOKEN = localStorage.getItem("token");
-    axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
-    Vue.prototype.$http = axios
+
+    // 请求拦截器
+    axios.interceptors.request.use(function (config) {
+        // 所有请求发起之后,进行筛选,请求标识是不是login
+        // 如果标识是login,不要头部->请求继续发起
+        // 当请求标识不是login,先设置头部,再发送请求
+        if (config.url !== "login") {
+            const AUTH_TOKEN = localStorage.getItem("token");
+            // axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+            config.headers["Authorization"] = AUTH_TOKEN;
+        }
+        return config;
+    }, function (error) {
+        // 对请求错误做些什么
+        return Promise.reject(error);
+    });
+
+    // const AUTH_TOKEN = localStorage.getItem("token");
+    // axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
+    Vue.prototype.$http = axios;
 
 }
 export default HttpServer;
